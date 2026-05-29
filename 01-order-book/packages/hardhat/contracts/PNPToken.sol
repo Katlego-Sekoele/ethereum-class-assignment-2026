d80283801b6c35868bb0e33b9eea376075c1920c
Katlego Sekoele <sekoelekatlego@gmail.com>
2026-05-29 15:45:26 +0200

    implement FNBT and PNPT

--- 01-order-book/packages/hardhat/contracts/PNPToken.sol
+++ 01-order-book/packages/hardhat/contracts/PNPToken.sol
@@ -1,1 +1,10 @@
-contract PNPToken {}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PNPToken is ERC20 {
+    // inherit ERC20, change token name and token symbol
+    constructor(uint256 initialSupply_) ERC20("PNP Token", "PNPT") {
+        _mint(msg.sender, initialSupply_);
+    }
+}
